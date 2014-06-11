0cf5961f6ca248f5cb8d8f8cf902b2a04337cc8c
sibojia <evergreen132@126.com>
2014-06-12 00:08:24 +0800

    added getblock logic

--- src/app/static/index.js
+++ src/app/static/index.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 	$(".block").fadeIn("slow");
+	$(".block").each(function(){
+		$(this).load('/getblock/'+$(this).attr('id'));
+	});
+	// $.get('/getblock/'+, {
+ //        text: $(sourceId).text(),
+ //        sourceLang: sourceLang,
+ //        destLang: destLang
+ //    }).done(function(translated) {
+ //        $(destId).text(translated['text'])
+ //        $(loadingId).hide();
+ //        $(destId).show();
+ //    }).fail(function() {
+ //        $(destId).text("{{ _('Error: Could not contact server.') }}");
+ //        $(loadingId).hide();
+ //        $(destId).show();
+ //    });
 	// $(".block").mouseenter(function(){
 	// 	$(this).addClass("hover");
 	// });
