4a9744d4783e4107251e1a556746d0cb4a0522ae
Glaceon31 <grit31@126.com>
2014-06-13 00:53:09 +0800

    nothigni?

--- src/app/views_getblock.py
+++ src/app/views_getblock.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, flash, redirect, session, request, g, make_response
 from app import app, database, block
 from modules import *
+from tools import *
 import json
 
 services = {'baidu.news': TestGrabber.TestGrabber(), 'renren.status': RenrenGrabber.RenrenGrabber()}
+tools = {'index': Index.Index(), 'image': Image.Image()}
 
 @app.route('/getblock/<blockId>', methods = ['GET', 'POST'])
 def getblock(blockId):
@@ -11,6 +13,8 @@ def getblock(blockId):
 	b = g.blocks[int(blockId[1:]) - 1]
 	if services.has_key(b.name):
 		b.content['raw'] = json.loads(getservice(b.name))
+	elif tools.has_key(b.name):
+                return tools[b.name].render(b.config)
 	else:
 		b.content['raw'] = 'Default'
 	return render_template('block_textlines.html', block = b)
