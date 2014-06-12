d4c58c764ac1a8655ff804c8bdb0c2dc95d09a0b
Glaceon31 <grit31@126.com>
2014-06-13 01:19:26 +0800

    image1

--- src/app/views_getblock.py
+++ src/app/views_getblock.py
@@ -11,6 +11,7 @@ tools = {'image': Image.Image()}
 def getblock(blockId):
 	# b = block.Block(1,1,'test')
 	b = g.blocks[int(blockId[1:]) - 1]
+	print session['ihomepage']
 	if services.has_key(b.name):
 		b.content['raw'] = json.loads(getservice(b.name))
 	elif tools.has_key(b.name):
